a9af24a0fb43481c247e7448e0fa1f6fe7675677
Kelly Mahan <kelly@digimedia.com>
2009-04-14 09:11:00 -0500

    update to memcache

--- lib/memcachedb_q.rb
+++ lib/memcachedb_q.rb
@@ -9,24 +9,25 @@ end
 
 class MemcachedbQ
   
-  CACHE_DB = MemCacheDb.new 'localhost:21201', :namespace => 'cart'
   
+  attr_accessor :cache_db
   attr_accessor :que_name
   
   def initialize(name)
+    @cache_db = MemCacheDb.new 'localhost:21201', :namespace => 'cart'
     @que_name = name.to_s
   end
   
   def add(data, que_time = Time.now)
-    CACHE_DB.set("#{@que_name}-#{que_time.to_f.to_s}-#{rand(9999)}", data.to_yaml)
+    @cache_db.set("#{@que_name}-#{que_time.to_f.to_s}-#{rand(9999)}", data.to_yaml)
   end
   
   def remove(key)
-    CACHE_DB.delete(key)
+    @cache_db.delete(key)
   end
   
   def next
-    item = CACHE_DB.get_range("#{@que_name}", "#{@que_name}-#{Time.now.to_f.to_s}~", 0, 1, 1)
+    item = @cache_db.get_range("#{@que_name}", "#{@que_name}-#{Time.now.to_f.to_s}~", 0, 1, 1)
     if item
       data = item[item.keys[0]].f_yaml
       self.remove(item.keys[0])
@@ -38,10 +39,10 @@ class MemcachedbQ
   
   def get_que
     totalque = {}
-    temp_que = CACHE_DB.get_range("#{@que_name}", "#{@que_name}-#{Time.now.to_f.to_s}~")
+    temp_que = @cache_db.get_range("#{@que_name}", "#{@que_name}-#{Time.now.to_f.to_s}~")
     totalque.merge!(temp_que)
     while temp_que ? temp_que.length == 100 : false 
-      temp_que = CACHE_DB.get_range("#{que.keys.sort.last}", "#{@que_name}-#{Time.now.to_f.to_s}~", 1)
+      temp_que = @cache_db.get_range("#{que.keys.sort.last}", "#{@que_name}-#{Time.now.to_f.to_s}~", 1)
       totalque.merge!(temp_que)
     end
     return totalque
@@ -49,10 +50,10 @@ class MemcachedbQ
   
   def get_future_que
     totalque = {}
-    temp_que = CACHE_DB.get_range("#{@que_name}-#{Time.now.to_f.to_s}", "#{@que_name}~", 1)
+    temp_que = @cache_db.get_range("#{@que_name}-#{Time.now.to_f.to_s}", "#{@que_name}~", 1)
     totalque.merge!(temp_que)
     while temp_que ? temp_que.length == 100 : false 
-      temp_que = CACHE_DB.get_range("#{que.keys.sort.last}", "#{@que_name}~", 1)
+      temp_que = @cache_db.get_range("#{que.keys.sort.last}", "#{@que_name}~", 1)
       totalque.merge!(temp_que)
     end
     return totalque
